c8c7c48b88bb5ee1017fcf953bb19f098f390fc6
Emilie Douvier <emilie@MacBook-Air-de-Emilie.local>
2018-11-08 17:04:53 +0100

    first

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,32 @@
+require 'uri'
+
+class MoviesController < ApplicationController
+
+  def search
+  end
+
+  def result
+    $results = SearchMovie.new(params[:movie][:search])
+    @movie_params = params[:movie][:search]
+    p @movie_params
+    if @movie_params.length > 0
+      $display = $results.result
+      new_uri
+      redirect_to root_path
+    else
+      flash[:danger] = 'Search cannot be empty'
+      redirect_to root_path
+    end
+
+  end
+
+  private
+  def new_uri
+    if !$display.nil?
+      $display.each do |result|
+        result.poster_path = "https://image.tmdb.org/t/p/w500" + result.poster_path.to_s unless   result.poster_path.nil?
+      end
+    end
+  end
+
+end
---
+++ app/services /search_movie.rb
@@ -0,0 +1,21 @@
+
+require 'dotenv'
+
+Dotenv.load
+
+class SearchMovie
+
+	def initialize(movie)
+    @movie = movie
+  end
+
+  def log_in
+    Tmdb::Api.key(ENV['MOVIE_DB_KEY'])
+  end
+
+  def result
+    log_in
+    Tmdb::Movie.find(@movie)
+  end
+
+end
